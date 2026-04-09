336d836aad826332876ae25b0697934cd8106fd2
Maaz Khan <maazkhanm9@gmail.com>
2026-04-09 12:18:24 +0530

    Keep only CareerFlow test

--- tests/mytest.spec.ts
+++
@@ -1,8 +0,0 @@
-import {test,expect} from "@playwright/test";
-test("verify page title",async ({page})=>{
-await page.goto("https://automationexercise.com/#google_vignette");
-await expect(page).toHaveTitle("Automation Exercise");
- 
-
-
-})
