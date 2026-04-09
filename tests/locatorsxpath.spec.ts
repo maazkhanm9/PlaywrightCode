107baf378e2496bd172295f0b32968b5a2a32f80
Maaz Khan <maazkhanm9@gmail.com>
2026-04-09 12:18:24 +0530

    Keep only CareerFlow test

--- tests/locatorsxpath.spec.ts
+++
@@ -1,8 +0,0 @@
-import{test,expect, Locator} from "@playwright/test"
-test("Xpath Axes demo",async({page})=>{
-
-page.goto('https://rahulshettyacademy.com/blog/index.php/2023/06/29/playwright-interview-questions/');
-const SubscribeButton:Locator=page.locator("//button[text()='Subscribe Now']");
-await expect(SubscribeButton).toHaveText('Subscribe Now');
-
-})
